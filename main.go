1734a8d62299f021d0eda9cd0df89f422bf107d9
Shubham <shubham16290@gmail.com>
2025-06-15 00:08:52 +0530

    db-initialized-in-main-and-added-accessToken-in-user-model

--- main.go
+++ main.go
@@ -3,21 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gitshubham45/brokerPlatformApi/db"
+	"github.com/gitshubham45/brokerPlatformApi/routes"
 )
 
 func main() {
 	fmt.Println("stock broker platform")
 
+	db.Init()
+	
 	r := gin.Default()
 
 	r.GET("health" , func(c *gin.Context){
 		c.JSON(http.StatusOK, gin.H{"health" : "ok"})
 	})
 
+	api := r.Group("/api")
+
+	routes.UserRoute(api)
+
 	port := "8080"
-	
+
+	port = os.Getenv("PORT")
 
 	r.Run(":" + port)
 }
--- models/user.go
+++ models/user.go
@@ -7,6 +7,7 @@ type User struct {
 	Email        string    `josn:"email" bson:"email"`
 	PasswordHash string    `json:"passwordHash" bson:"passwordHash"`
 	RefreshToken string    `json:"refreshToken" bson:"refreshToken"`
+	AccessToken  string    `json:"accessToken" bson:"accessToken"`
 	CreatedAt    time.Time `json:"createdAt" bson:"createdAt"`
 	UpdatedAt    time.Time `json:"updatedAt" bson:"updatedAt"`
 }
